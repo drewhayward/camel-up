bcd7cd49372534b3e879c75d118517bf9d461441
Drew Hayward <d.andrew.hayward@gmail.com>
2020-11-26 13:37:20 -0500

    init commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# `camel-up`
+
+A partial python implementation of the wonderful game [Camel Up](https://www.amazon.com/Eggertspiele-PBGESG50120EN-Camel-Board-Game/dp/B07HB88VXG/ref=sr_1_1?dchild=1&keywords=camel+up&qid=1606415483&s=toys-and-games&sr=1-1). Please buy the real game!
+
+The program allows the movement of camels and placement of spectator tiles. It also calculates the true-probability of each camel winning the current leg.
+
+## TODO:
+- Add crazy camel execptions (always unstack, always move the laden crazy camel)
+- Add crazy camels to leg simulations
+- Add mid-leg odds calculations
+- Create nicer CLI game display and interface
---
+++ camel.py
@@ -0,0 +1,157 @@
+import random
+from copy import deepcopy
+from tqdm import tqdm
+from itertools import permutations, product
+
+BOARD_SIZE = 16
+GOOD_CAMELS = {'r', 'b', 'g', 'y', 'p'}
+CRAZY_CAMELS = {'k' ,'w'}
+CAMEL_DIR = {
+    'r': 1,
+    'b': 1,
+    'g': 1,
+    'y': 1,
+    'p': 1,
+    'k': -1,
+    'w': -1
+}
+
+class Board():
+    def __init__(self):
+        self.board = [[] for _ in range(BOARD_SIZE)]
+        self.cheering_tiles = [0 for _ in range(BOARD_SIZE)]
+
+    def print_board(self):
+        for height in reversed(range(len(GOOD_CAMELS) + len(CRAZY_CAMELS))):
+            line = ''
+            for pos in range(BOARD_SIZE):
+                if height < len(self.board[pos]):
+                    line += self.board[pos][height].upper()
+                else:
+                    line += ' '
+            print(line)
+
+        print(''.join(['-' for _ in range(BOARD_SIZE)]))
+        line = ''
+        for i in range(BOARD_SIZE):
+            line += str(i)[-1]
+        print(line)
+
+        line = ''
+        for i in range(BOARD_SIZE):
+            if self.cheering_tiles[i] == 1:
+                line += '+'
+            elif self.cheering_tiles[i] == -1:
+                line += '-'
+            else:
+                line += ' '
+
+        print(line)
+        print('Current Leader:', self.leader())
+
+    def leader(self):
+        for i in reversed(range(BOARD_SIZE)):
+            if self.board[i] and GOOD_CAMELS.intersection(self.board[i]):
+                for camel in reversed(self.board[i]):
+                    if camel in GOOD_CAMELS:
+                        return camel
+                
+        raise Exception('No good camels found')
+
+    def random_board(self):
+        self.board = [[] for _ in range(BOARD_SIZE)]
+        self.cheering_tiles = [0 for _ in range(BOARD_SIZE)]
+
+        for camel in GOOD_CAMELS:
+            self.place_camel(camel, random.randint(0,2))
+
+        for camel in CRAZY_CAMELS:
+            self.place_camel(camel, (0 - random.randint(1,3)) % BOARD_SIZE)
+
+    def place_camel(self, camel, pos):
+        self.board[pos] += [camel]
+
+    def place_tile(self, pos, value):
+        if value == '+':
+            self.cheering_tiles[pos] = 1
+        else:
+            self.cheering_tiles[pos] = -1
+
+    def move(self, camel, hops):
+        new_pos = None
+        for i in range(len(self.board)):
+            if camel in self.board[i]:
+                camel_idx = self.board[i].index(camel)
+                stack = self.board[i][camel_idx:]
+                self.board[i] = self.board[i][:camel_idx]
+                new_pos = (i + (hops * CAMEL_DIR[camel])) % BOARD_SIZE
+                self.board[new_pos] += stack
+                break
+
+        if new_pos is None:
+            raise Exception(f"Couldn't find camel {camel}")
+
+        # Check for +/- 1 tile
+        if self.cheering_tiles[new_pos] != 0:
+            step = self.cheering_tiles[new_pos] * CAMEL_DIR[camel]
+            step_pos = (new_pos + step) % BOARD_SIZE
+
+            # Do strange hop thing
+            if self.cheering_tiles[new_pos] == -1:
+                self.board[step_pos] = self.board[new_pos] + self.board[step_pos]
+                self.board[new_pos] = []
+
+            else: # normal hops
+                self.board[step_pos] = self.board[step_pos] + self.board[new_pos]
+                self.board[new_pos] = []
+
+
+def simulate_leg(board: Board):
+    players = list(GOOD_CAMELS) + [None]
+    winners = {camel: 0 for camel in GOOD_CAMELS}
+    trials = 0
+    # For dice permutation
+    with tqdm(total=174960) as pbar:
+        for ig_player in (players):
+            small_players = players[:]
+            small_players.remove(ig_player)
+            
+            # For roll permutation
+            for camels in (permutations(small_players)):
+                for rolls in product(range(1,4), repeat=len(camels)):
+                    board_copy = deepcopy(board)
+                    trials += 1
+                    pbar.update(1)
+                    for camel, hops in zip(camels, rolls):
+                        if camel is not None:
+                            board_copy.move(camel, hops)
+                    winners[board_copy.leader()] += 1
+
+    print('Current Camel Odds:')
+    for camel, wins in winners.items():
+        print(f'\tCamel: {camel}, chance: {(wins/trials)*100}')
+                        
+
+
+
+if __name__ == "__main__":
+    board = Board()
+    board.random_board()
+
+    while True:
+        board.print_board()
+        simulate_leg(board)
+        print(f'Camels: {", ".join(GOOD_CAMELS.union(CRAZY_CAMELS))}')
+        print('Move a camel eg: b2, g1, k3')
+        print('Place a spectator tile. Eg: -3 +13')
+        move = input(f'Enter your move: ')
+
+        try:
+            hops = int(move[1:])
+            camel = move[:1]
+        except:
+            continue
+        if camel in {'+', '-'}:
+            board.place_tile(hops, camel)
+        else:
+            board.move(camel, hops)
